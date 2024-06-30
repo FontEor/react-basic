ae448b1e37483634c98b9ac2fd9c99d9e1d38870
BobLiu11 <583139315@qq.com>
2024-06-30 12:09:35 +0800

    feat: 列表渲染

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.scss'
-import avatar from './images/bozai.png'
+import { useState } from "react";
+import "./App.scss";
+import avatar from "./images/bozai.png";
 
 /**
  * 评论列表的渲染和操作
@@ -15,48 +16,48 @@ const defaultList = [
     rpid: 3,
     // 用户信息
     user: {
-      uid: '13258165',
-      avatar: '',
-      uname: '周杰伦',
+      uid: "13258165",
+      avatar,
+      uname: "周杰伦",
     },
     // 评论内容
-    content: '哎哟，不错哦',
+    content: "哎哟，不错哦",
     // 评论时间
-    ctime: '10-18 08:15',
+    ctime: "10-18 08:15",
     like: 88,
   },
   {
     rpid: 2,
     user: {
-      uid: '36080105',
-      avatar: '',
-      uname: '许嵩',
+      uid: "36080105",
+      avatar,
+      uname: "许嵩",
     },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
+    content: "我寻你千百度 日出到迟暮",
+    ctime: "11-13 11:29",
     like: 88,
   },
   {
     rpid: 1,
     user: {
-      uid: '30009257',
+      uid: "30009257",
       avatar,
-      uname: '黑马前端',
+      uname: "黑马前端",
     },
-    content: '学前端就来黑马',
-    ctime: '10-19 09:00',
+    content: "学前端就来黑马",
+    ctime: "10-19 09:00",
     like: 66,
   },
-]
+];
 // 当前登录用户信息
 const user = {
   // 用户id
-  uid: '30009257',
+  uid: "30009257",
   // 用户头像
   avatar,
   // 用户昵称
-  uname: '黑马前端',
-}
+  uname: "黑马前端",
+};
 
 /**
  * 导航 Tab 的渲染和操作
@@ -69,11 +70,12 @@ const user = {
 
 // 导航 Tab 数组
 const tabs = [
-  { type: 'hot', text: '最热' },
-  { type: 'time', text: '最新' },
-]
+  { type: "hot", text: "最热" },
+  { type: "time", text: "最新" },
+];
 
 const App = () => {
+  const [list, setList] = useState(defaultList);
   return (
     <div className="app">
       {/* 导航 Tab */}
@@ -86,12 +88,11 @@ const App = () => {
           </li>
           <li className="nav-sort">
             {/* 高亮类名： active */}
-            <span className='nav-item'>最新</span>
-            <span className='nav-item'>最热</span>
+            <span className="nav-item">最新</span>
+            <span className="nav-item">最热</span>
           </li>
         </ul>
       </div>
-
       <div className="reply-wrap">
         {/* 发表评论 */}
         <div className="box-normal">
@@ -113,45 +114,37 @@ const App = () => {
             </div>
           </div>
         </div>
-        {/* 评论列表 */}
         <div className="reply-list">
-          {/* 评论项 */}
-          <div className="reply-item">
-            {/* 头像 */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                />
+          {list.map((item) => (
+            <div key={item.rpid} className="reply-item">
+              <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                  <img
+                    className="bili-avatar-img"
+                    alt=""
+                    src={item.user.avatar}
+                  />
+                </div>
               </div>
-            </div>
-
-            <div className="content-wrap">
-              {/* 用户名 */}
-              <div className="user-info">
-                <div className="user-name">jack</div>
-              </div>
-              {/* 评论内容 */}
-              <div className="root-reply">
-                <span className="reply-content">这是一条评论回复</span>
-                <div className="reply-info">
-                  {/* 评论时间 */}
-                  <span className="reply-time">{'2023-11-11'}</span>
-                  {/* 评论数量 */}
-                  <span className="reply-time">点赞数:{1000}</span>
-                  <span className="delete-btn">
-                    删除
-                  </span>
-
+              <div className="content-wrap">
+                <div className="user-info">
+                  <div className="user-name">{item.user.uname}</div>
+                </div>
+                <div className="root-reply">
+                  <span className="reply-content">{item.content}</span>
+                  <div className="reply-info">
+                    <span className="reply-time">{item.ctime}</span>
+                    <span className="reply-time">点赞数:{item.like}</span>
+                    <span className="delete-btn">删除</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
