df4218d1bf32ce579c75f4db2e7ff2db3bbea3cd
BobLiu11 <583139315@qq.com>
2024-06-30 12:20:37 +0800

    feat: 删除功能

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import { useState } from "react";
 import "./App.scss";
 import avatar from "./images/bozai.png";
 
-/**
- * 评论列表的渲染和操作
- *
- * 1. 根据状态渲染评论列表
- * 2. 删除评论
- */
-
-// 评论列表数据
 const defaultList = [
   {
-    // 评论id
     rpid: 3,
-    // 用户信息
     user: {
       uid: "13258165",
       avatar,
       uname: "周杰伦",
     },
-    // 评论内容
     content: "哎哟，不错哦",
-    // 评论时间
     ctime: "10-18 08:15",
     like: 88,
   },
@@ -49,26 +37,11 @@ const defaultList = [
     like: 66,
   },
 ];
-// 当前登录用户信息
 const user = {
-  // 用户id
   uid: "30009257",
-  // 用户头像
   avatar,
-  // 用户昵称
   uname: "黑马前端",
 };
-
-/**
- * 导航 Tab 的渲染和操作
- *
- * 1. 渲染导航 Tab 和高亮
- * 2. 评论列表排序
- *  最热 => 喜欢数量降序
- *  最新 => 创建时间降序
- */
-
-// 导航 Tab 数组
 const tabs = [
   { type: "hot", text: "最热" },
   { type: "time", text: "最新" },
@@ -76,39 +49,35 @@ const tabs = [
 
 const App = () => {
   const [list, setList] = useState(defaultList);
+  const handleDel = (id) => {
+    setList(list.filter((item) => item.rpid !== id));
+  };
   return (
     <div className="app">
-      {/* 导航 Tab */}
       <div className="reply-navigation">
         <ul className="nav-bar">
           <li className="nav-title">
             <span className="nav-title-text">评论</span>
-            {/* 评论数量 */}
             <span className="total-reply">{10}</span>
           </li>
           <li className="nav-sort">
-            {/* 高亮类名： active */}
             <span className="nav-item">最新</span>
             <span className="nav-item">最热</span>
           </li>
         </ul>
       </div>
       <div className="reply-wrap">
-        {/* 发表评论 */}
         <div className="box-normal">
-          {/* 当前用户头像 */}
           <div className="reply-box-avatar">
             <div className="bili-avatar">
               <img className="bili-avatar-img" src={avatar} alt="用户头像" />
             </div>
           </div>
           <div className="reply-box-wrap">
-            {/* 评论框 */}
             <textarea
               className="reply-box-textarea"
               placeholder="发一条友善的评论"
             />
-            {/* 发布按钮 */}
             <div className="reply-box-send">
               <div className="send-text">发布</div>
             </div>
@@ -135,7 +104,14 @@ const App = () => {
                   <div className="reply-info">
                     <span className="reply-time">{item.ctime}</span>
                     <span className="reply-time">点赞数:{item.like}</span>
-                    <span className="delete-btn">删除</span>
+                    {item.user.uid === user.uid && (
+                      <span
+                        className="delete-btn"
+                        onClick={() => handleDel(item.rpid)}
+                      >
+                        删除
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
