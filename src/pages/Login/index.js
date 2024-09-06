6924ac35635d22cdc2dcfe7180e1fe57300f7ed1
BobLiu11 <583139315@qq.com>
2024-09-06 10:20:16 +0800

    feat: 登录页

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,45 @@
-function Login() {
-  return <div>登录页面</div>;
-}
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+// import logo from "@/assets/logo.png";
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <h1 className="login-logo">登录</h1>
+        {/* <img className="login-logo" src={logo} alt="" /> */}
+        {/* 登录表单 */}
+        <Form validateTrigger={["onBlur"]} onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号码格式不对",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
 export default Login;
--- src/vie/Channel.js
+++ src/vie/Channel.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch, Provider } from "react-redux";
 import { useState, useRef, useEffect } from "react";
 
-import { fetchChannelLsit } from "../store/modules/channelStore";
+import { fetchChannelLsit } from "@/store/modules/channelStore";
 const Channle = () => {
   const dispatch = useDispatch();
   useEffect(() => {
